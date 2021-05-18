9f81bfa4e3075f752c292ac9544e5587793f6541
YSH <sheepsh93@gmail.com>
2021-05-18 20:50:55 +0900

    PostDetail페이지
    PostDetailPage에 다수의 이미지 추가 및 장바구니 이동기능

--- client/src/components/views/PostPage/PostDetailPage.js
+++ client/src/components/views/PostPage/PostDetailPage.js
@@ -1,10 +1,9 @@
 
 import { Container, CssBaseline, Grid, makeStyles, Typography, ButtonBase, Box, Button, List, ListItem, ListItemText, Divider, FormHelperText, FormControl, ButtonGroup } from '@material-ui/core'
 
-import TextField from '@material-ui/core/TextField'
+
 import React, { useState,useEffect } from 'react'
-import { LockOutlined, SlowMotionVideo, SortOutlined } from '@material-ui/icons'
-import CopyrightFooter from '../CopyrightFooter/CopyrightFooter'
+
 
 import { useDispatch } from 'react-redux';
 import SwipeableViews from 'react-swipeable-views';
@@ -14,20 +13,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-
-import {addCart} from '../../../_action/user_actions'
-
-import { registerUser } from '../../../_action/user_actions'
 import { useHistory } from 'react-router';
-import Paper from '@material-ui/core/Paper';
+
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-
-import ex1 from './img/1.jpg'
-import ex2 from './img/2.jpg'
-import ex0 from './img/0.png'
-import ex4 from './img/4.png'
-import red from './img/red.png';
 
 
 import ToggleButton from '@material-ui/lab/ToggleButton';
@@ -110,7 +99,7 @@ export default function PostDetailPage(props) {
     const theme = useTheme();
 
     const [image,setImage] =useState([])
-    const [post, setPost] = useState({})
+    const [post, setPost] = useState({}) //sh 214
     const postId = props.match.params.postId
      useEffect(() => {
  
@@ -118,7 +107,7 @@ export default function PostDetailPage(props) {
          .then(response => {
              console.log(response.data.post[0])
               setPost(response.data.post[0])
-              setImage(response.data.post[0].image)
+              setImage(response.data.post[0].image) //sh 214  // 281~284
          })
      }, [])
    
@@ -130,11 +119,10 @@ export default function PostDetailPage(props) {
 
     const history = useHistory();
     
-
-    //sh-239
+    //sh-254 장바구니 누르면 해당 데이터를 Cart에 넣고 해당 유저의 장바구니 페이지로 이동
     const clickCartHandler = () => {
-
-      let user =props.user.userData
+      
+      let user = props.user
             let body = {
                 post: postId,
                 size: size,
@@ -143,8 +131,11 @@ export default function PostDetailPage(props) {
             console.log(body)
             axios.post('/api/cart/create',body)
             .then(response => {
-                if(response.data.success) {
-                  // alert('장바구니에 해당 상품을 추가했습니다.')
+                if(response.data.status) {
+                  history.push({
+                    pathname: '/shoppingbascket',
+                    state:{user:user}
+                  })
                 }
             })
     }
@@ -180,7 +171,7 @@ export default function PostDetailPage(props) {
     const[quantity,setQuantity] = useState(1)
  
     const handleMinusQuantityChange = (event) => {
-      if(quantity >0) {
+      if(quantity >1) {
         setQuantity(quantity - 1)
       }
     }
@@ -194,14 +185,11 @@ export default function PostDetailPage(props) {
         setSize(event.target.value);
     };
 
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(1);
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-
- 
-    console.log(image)
     return (
 
       
@@ -250,7 +238,7 @@ export default function PostDetailPage(props) {
 
             </FormControl>
             <ButtonGroup variant="text" fullWidth="true">
-                <Button variant="outlined" style={{fontSize:'1.2rem'}} aria-label="장바구니" onClick={clickCartHandler} href = '/shoppingbascket'>장바구니</Button>
+                <Button variant="outlined" style={{fontSize:'1.2rem'}} aria-label="장바구니" onClick={clickCartHandler}>장바구니</Button>
                 <Button variant="outlined" style={{fontSize:'1.2rem'}}  aria-label="바로구매">바로구매</Button>
                 </ButtonGroup>
             </Grid>
@@ -279,9 +267,9 @@ export default function PostDetailPage(props) {
         <TabPanel value={value} index={0} dir={theme.direction}>
         <Box component="span" m={1}><Button /></Box>
         <Box width="100%"><img className={classes.img} alt="complex" src={image[1]} /></Box>
-        <Box width="100%"><img className={classes.img} alt="complex" src={ex4} /></Box>
-        <Box width="100%"><img className={classes.img} alt="complex" src={ex1} /></Box>
-        <Box width="100%"><img className={classes.img} alt="complex" src={ex2} /></Box>
+        <Box width="100%"><img className={classes.img} alt="complex" src={image[2]} /></Box>
+        <Box width="100%"><img className={classes.img} alt="complex" src={image[3]} /></Box>
+        <Box width="100%"><img className={classes.img} alt="complex" src={image[4]} /></Box>
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
           음성리뷰창
