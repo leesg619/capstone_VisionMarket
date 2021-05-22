bf2b4ae6122d86bbb7b4cefac6993e8e461ea935
Beans9605 <alkad1234@likelion.org>
2021-05-22 18:35:25 +0900

    0522_YeongFix

---
+++ client/src/components/OCRkey.js
@@ -0,0 +1,19 @@
+export const NaverOCR_KEY = "https://85db3bce72a7459d8cf672cb9af18928.apigw.ntruss.com/custom/v1/8263/ed7588280ddea2b1a1c8a51aefee5fb96e21e0d40267c0857bb5da8acd6de97f/general"
+
+/*  OCR 이미지 날리는 방식
+{
+    "images": [
+      {
+        "format": "jpeg",
+        "name": "medium",
+        "data": null,
+        "url": "https://thumbnail8.coupangcdn.com/thumbnails/remote/q89/image/retail/images/615119903589168-4faaa5f6-1722-4ee1-bd1b-d210cf45d900.jpg"
+      }
+    ],
+    "lang": "ko",
+    "requestId": "string",
+    "resultType": "string",
+    "timestamp": {{$timestamp}},
+    "version": "V1"
+}
+*/
